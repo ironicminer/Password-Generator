ba4f058c010bbc14dc22ec89acee5e519c191220
nate <rotarygurter@gmail.com>
2020-10-16 22:30:33 -0500

    added comments and changed color

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Generate password function
 function generatePassword() {
+  let passVal = [];
   let length = prompt("How many characters?");
-  let passVal = "";
-  let charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
-  for (var i = 0; i < length; i++) {
-    let c = Math.floor(Math.random() * charset.length + 1);
-    passVal += charset.charAt(c);
+  let charsUpper = prompt("Uppercase?");
+  let charsLower = prompt("Lowercase?");
+  // Checking the length of password input
+  if (length < !8 && length > !128);
+  {
+    prompt("Not the correct amount of characters!");
+    {
+    }
+    if (length > !8 && length < !128);
+    {
+      confirm("Special Characters?");
+    }
+
+    let charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+    //Loop to generate random password from character set
+    for (var i = 0; i < length; i++) {
+      let c = Math.floor(Math.random() * charset.length + 1);
+      passVal += charset.charAt(c);
+    }
   }
   return passVal;
 }
+// if (special === true);
+// {
+//   console.log("works");
 
 //return passVal;
 
