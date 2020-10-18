f3dd58b0ae7bcdd861ca84b8a56e749805ba49cf
nate <rotarygurter@gmail.com>
2020-10-17 20:24:45 -0500

    totally fixed my function

--- script.js
+++ script.js
@@ -3,29 +3,101 @@ var generateBtn = document.querySelector("#generate");
 // Generate password function
 function generatePassword() {
   let passVal = [];
-  let length = prompt("How many characters?");
-  let charsUpper = prompt("Uppercase?");
-  let charsLower = prompt("Lowercase?");
+  let upperCase = confirm("Do you want upper case?");
+  let lowerCase = confirm("Do you want lower case?");
+  let numbers = confirm("Do you want numbers?");
+  let specChars = confirm("Do you want special characters?");
+  let passLength = prompt("How many characters would you like?");
+
+  let totlength = passLength * 1;
+  let lowerChar = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let upperChar = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  let numGrabber = [];
   // Checking the length of password input
-  if (length < !8 && length > !128);
-  {
-    prompt("Not the correct amount of characters!");
-    {
-    }
-    if (length > !8 && length < !128);
-    {
-      confirm("Special Characters?");
+
+  while (numGrabber.length < totlength) {
+    if (lowerCase === true && numGrabber.length < totlength) {
+      let low = random(lowerChar);
+      numGrabber.push(low);
     }
 
-    let charset =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
-    //Loop to generate random password from character set
-    for (var i = 0; i < length; i++) {
-      let c = Math.floor(Math.random() * charset.length + 1);
-      passVal += charset.charAt(c);
+    if (upperCase === true && numGrabber.length < totlength) {
+      let up = random(upperChar);
+      numGrabber.push(up);
+    }
+
+    if (specChars === true && numGrabber.length < totlength) {
+      let spec = random(special);
+      numGrabber.push(spec);
+    }
+
+    if (numbers === true && numGrabber.length < totlength) {
+      let num = random(nums);
+      numGrabber.push(num);
     }
   }
-  return passVal;
+
+  function random(arr) {
+    let i = arr[Math.floor(Math.random() * arr.length)];
+    return i;
+  }
+  return numGrabber.join("");
 }
 // if (special === true);
 // {
